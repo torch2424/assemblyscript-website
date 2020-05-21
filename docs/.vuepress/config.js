5f7a7158d63a54869d36f73ad76f84f958c06fd0
dcode <dcode@dcode.io>
2020-05-21 06:06:45 +0200

    Details & Favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,6 +11,17 @@ module.exports = {
       description: 'Definitely not a TypeScript to WebAssembly compiler 🚀',
     }
   },
+  head: [
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/favicons/apple-touch-icon.png"} ],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicons/favicon-32x32.png"} ],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicons/favicon-16x16.png"} ],
+    ['link', { rel: "manifest", href: "/site.webmanifest"} ],
+    ['link', { rel: "mask-icon", href: "/favicons/safari-pinned-tab.svg", color: "#007acc"} ],
+    ['link', { rel: "shortcut icon", href: "/favicon.ico"} ],
+    ['meta', { name: "msapplication-TileColor", content: "#ffffff"} ],
+    ['meta', { name: "msapplication-config", content: "/browserconfig.xml"} ],
+    ['meta', { name: "theme-color", content: "#ffffff"} ],
+  ],
   themeConfig: {
     logo: '/images/icon.svg',
     nav: [
@@ -112,7 +123,6 @@ module.exports = {
         },
         {
           title: 'The Details',
-          path: '/details/',
           collapsable: false,
           children: [
             'details/compiler',
--- docs/.vuepress/plugin.js
+++ docs/.vuepress/plugin.js
@@ -6,7 +6,7 @@ require("prismjs/components/prism-typescript");
 Prism.languages.ts =
 Prism.languages.typescript = Prism.languages.extend('typescript', {
 	// 'keyword': /\b(?:abstract|as|async|await|break|case|catch|class|const|constructor|continue|debugger|declare|default|delete|do|else|enum|export|extends|finally|for|from|function|get|if|implements|import|in|instanceof|interface|is|keyof|let|module|namespace|new|null|of|package|private|protected|public|readonly|return|require|set|static|super|switch|this|throw|try|type|typeof|undefined|var|void|while|with|yield)\b/,
-	'builtin': /\b(?:string|number|boolean|Array|Map|Set|symbol|Promise|i8|i16|i32|i64|isize|u8|u16|u32|u64|usize|bool|v128|anyref|auto)\b/,
+	'builtin': /\b(?:string|number|boolean|Array|Map|Set|symbol|Promise|i8|i16|i32|i64|isize|u8|u16|u32|u64|usize|bool|f32|f64|v128|anyref|auto)\b/,
 });
 
 module.exports = (options, ctx) => {
